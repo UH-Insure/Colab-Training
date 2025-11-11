0b72f53e38204626e653697b227aec18792f3197
jo5huar3 <jrlengineer@outlook.com>
2025-11-11 14:43:00 -0600

    Organize Repo

--- model/test.py
+++ model/test.py
@@ -1,4 +1,6 @@
-
-#
-def test():
-    print("Hello World!")
+import os
+import cryptol
+cry = cryptol.connect(url=os.environ.get("URL","https://cryptol-api-668187694923.us-central1.run.app:8080/"), reset_server=True)
+#cry = cryptol.connect(url="https://cryptol-api-668187694923.us-central1.run.app:8080/", reset_server=False)
+hello = cry.eval_f("2 + 2")
+print(hello.result())
